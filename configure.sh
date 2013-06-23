b6ba951601885b310e7ef1f8dff6fce828f53881
Lewis Brown <lewix7@hotmail.com>
2013-06-23 16:16:22 +0100

    Remove unused code in configure.sh
    Neither MONO nor RESULT are ever used.

--- configure.sh
+++ configure.sh
@@ -1,26 +1,5 @@
 #! /bin/bash -e
 
-# ------------------------------------------------------------------------------
-# Parse command line arguments and specify default values
-
-MONO=mono
-
-while getopts e:f:c:n OPT; do
-  case "$OPT" in
-    e) MONO=$OPTARG
-       ;;
-    n) MONO=""
-       ;;
-  esac
-done
-
-# ------------------------------------------------------------------------------
-# Utility function that searches specified directories for a specified file
-# and if the file is not found, it asks user to provide a directory
-
-RESULT=""
-
-
 # On OSX use Mono's private copy of pkg-config if it exists, see https://github.com/fsharp/fsharp/issues/107
 osx_pkg_config=/Library/Frameworks/Mono.framework/Versions/Current/bin/pkg-config
 if test -e $osx_pkg_config; then
