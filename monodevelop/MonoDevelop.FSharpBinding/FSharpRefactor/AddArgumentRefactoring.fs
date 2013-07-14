8d0d0dff8df863289bc2663284d35b8d91a495e4
Lewis Brown <lewix7@hotmail.com>
2013-07-14 14:17:56 +0200

    Add a dialog for choosing AddArgument parameters

--- monodevelop/MonoDevelop.FSharpBinding/FSharpRefactor/AddArgumentRefactoring.fs
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpRefactor/AddArgumentRefactoring.fs
@@ -1,7 +1,10 @@
 namespace MonoDevelop.FSharpRefactor
 
+open System
+open MonoDevelop.Ide
 open MonoDevelop.Refactoring
 
+open MonoDevelop.FSharp.Gui
 open MonoDevelop.FSharpRefactor.FSharpRefactoring
 open FSharpRefactor.Engine.Ast
 open FSharpRefactor.Refactorings
@@ -18,6 +21,25 @@ type AddArgumentRefactoring() as self =
                 (AddArgument.IsValid (Some position, None, None))
 
     override self.PerformChanges (options, properties) =
+        let parameters = properties :?> string array
         let position = GetPosition options
-        let refactorSource = AddArgument.Transform (position, "testName", "value")
+        let refactorSource =
+            AddArgument.Transform (position, parameters.[0], parameters.[1])
         PerformChanges (options, properties) refactorSource
+
+    override self.Run(options) =
+        let position = GetPosition options
+        let source, _ = GetSourceAndFilename options
+        let addArgumentIsValid name value = 
+            IsValid options
+                    (AddArgument.IsValid (Some position, Some name, Some value))
+        let getErrorMessage name value =
+            GetErrorMessage options
+                            (AddArgument.GetErrorMessage (Some position, Some name, Some value))
+        let itemDialog =
+            new AddArgumentParametersDialog(
+                self,
+                options,
+                new Func<String, String, bool>(addArgumentIsValid), 
+                new Func<String, String, string>(getErrorMessage))
+        MessageService.ShowCustomDialog(itemDialog) |> ignore
--- monodevelop/MonoDevelop.FSharpBinding/Gui/AddArgumentParametersDialog.cs
+++ monodevelop/MonoDevelop.FSharpBinding/Gui/AddArgumentParametersDialog.cs
@@ -1,10 +1,21 @@
 using System;
+using MonoDevelop.Refactoring;
 
 namespace MonoDevelop.FSharp.Gui
 {
-	public class EmptyClass
+	public class AddArgumentParametersDialog : RefactoringParametersDialog
 	{
-		public EmptyClass ()
+		public AddArgumentParametersDialog (RefactoringOperation refactoring,
+		                                    RefactoringOptions options,
+		                                    Func<String, String, bool> validateNameAndValue,
+		                                    Func<String, String, String> getErrorMessage)
+			: base(refactoring,
+			       options,
+			       new String[2] { "Argument na_me:", "Default value:" },
+			       new String[2] { "argumentName", "value" },
+			       (parameters) => validateNameAndValue(parameters[0], parameters[1]),
+			       (parameters) => getErrorMessage(parameters[0], parameters[1]))
+
 		{
 		}
 	}
