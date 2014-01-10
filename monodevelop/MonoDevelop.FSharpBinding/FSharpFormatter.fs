f54cb61d3057b412798813f57b4bd68d26b87283
Anh-Dung Phan <phananhdung309@yahoo.com>
2014-01-10 12:22:02 +0100

    Get formatting selection to work
    There are still issues with fsi files and range expansion.

--- monodevelop/MonoDevelop.FSharpBinding/FSharpFormatter.fs
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpFormatter.fs
@@ -10,54 +10,75 @@ open Fantomas
 open Fantomas.FormatConfig
 open Microsoft.FSharp.Compiler
 
+#if DEBUG
+type Debug = Console
+#endif
+
 type FormattingOption =
     | Document
     | Selection of int * int
 
 type FSharpFormatter() = 
-    let offsetToPos lines offset =
-        let rec loop i j = 
-            if i >= Array.length lines then None
-            elif lines.[i] >= j then Some (Range.mkPos i j)
-            else loop (i + 1) (j - lines.[i])
-        loop 0 offset
+    let offsetToPos (positions : _ []) offset =
+        let rec searchPos start finish = 
+            if start >= finish then None
+            elif start + 1 = finish then
+                Some (Range.mkPos (start + 1) (offset - positions.[start]))  
+            else
+                let mid = (start + finish) / 2
+                if offset = positions.[mid] then Some (Range.mkPos (mid + 1) 0)
+                elif offset > positions.[mid] then searchPos mid finish
+                else searchPos start mid
+        
+        searchPos 0 (positions.Length - 1)
 
-    let format (textStylePolicy : TextStylePolicy) (formattingPolicy : FSharpFormattingPolicy) input formattingOption =
+    let format isFsiFile (textStylePolicy : TextStylePolicy) (formattingPolicy : FSharpFormattingPolicy) input formattingOption =
         let config = FormatConfig.Default
         match formattingOption with
         | Document -> 
-            // TODO: Need to determine current input is F# implementation or signature file
-            defaultArg (CodeFormatter.tryFormatSourceString false input config) input
+            defaultArg (CodeFormatter.tryFormatSourceString isFsiFile input config) input
 
         | Selection(fromOffset, toOffset) ->
             // Convert from offsets to line and column position
-            let lines = 
+            let positions = 
                 input.Replace("\r\n", "\n").Replace("\r", "\n").Split('\n')
                 |> Seq.map (fun s -> String.length s + 1)
                 |> Seq.scan (+) 0
                 |> Seq.toArray
             let fromOffset = max 0 fromOffset
             let toOffset = min input.Length toOffset
-            let startPos = offsetToPos lines fromOffset
-            let endPos = offsetToPos lines toOffset
-            Debug.Assert(startPos.IsSome && endPos.IsSome, "Offsets are within valid ranges.")
+            Debug.WriteLine("**Fantomas**: Offsets from {0} to {1}", fromOffset, toOffset)
+            let startPos = offsetToPos positions fromOffset
+            let endPos = offsetToPos positions toOffset
+            Diagnostics.Debug.Assert(startPos.IsSome && endPos.IsSome, "Offsets are within valid ranges.")
             let r = Range.mkRange "/tmp.fsx" startPos.Value endPos.Value
-            let output = defaultArg (CodeFormatter.tryFormatSelectionFromString false r input config) input
+            Debug.WriteLine("**Fantomas**: Try to format range {0}.", r)
+            // This is not going to be sufficient if Fantomas expands the range for valid F# code.
+            // It should be better to get handle of the document and replace the whole text
+            let output = defaultArg (CodeFormatter.tryFormatSelectionFromString isFsiFile r input config) input
             let delta = input.Length - toOffset
             output.[fromOffset..output.Length - delta - 1]
+
+    let formatText (policyParent : PolicyContainer) (mimeTypeInheritanceChain : string seq) (input : string) formattingOption =
+        let isFsiFile = 
+            if Seq.isEmpty mimeTypeInheritanceChain then false
+            else 
+                let ext = Seq.head mimeTypeInheritanceChain
+                ext.ToLower() <> "text/x-fsharp"
+        Debug.WriteLine("**Fantomas**: Is this an fsi file? {0}", isFsiFile)
+        let textStylePolicy = policyParent.Get<TextStylePolicy>(mimeTypeInheritanceChain)
+        let formattingPolicy = policyParent.Get<FSharpFormattingPolicy>(mimeTypeInheritanceChain)
+
+        format isFsiFile textStylePolicy formattingPolicy input formattingOption
 
     // We don't support on-the-fly formatting and smart indenting yet. 
     // There's no point to use IAdvanceCodeFormatter.
     interface ICodeFormatter with
-        member __.FormatText(policyParent : PolicyContainer, mimeTypeInheritanceChain : string seq, input : string) =
-            Debug.WriteLine("Formatting document")
-            let textStylePolicy = policyParent.Get<TextStylePolicy>(mimeTypeInheritanceChain)
-            let formattingPolicy = policyParent.Get<FSharpFormattingPolicy>(mimeTypeInheritanceChain)
-            format textStylePolicy formattingPolicy input Document
+        member __.FormatText(policyParent, mimeTypeInheritanceChain, input) =
+            Debug.WriteLine("**Fantomas**: Formatting document")
+            formatText policyParent mimeTypeInheritanceChain input Document
 
-        member __.FormatText(policyParent : PolicyContainer, mimeTypeInheritanceChain : string seq, input : string, fromOffset : int, toOffset : int) =
-            Debug.WriteLine("Formatting selection")
-            let textStylePolicy = policyParent.Get<TextStylePolicy>(mimeTypeInheritanceChain)
-            let formattingPolicy = policyParent.Get<FSharpFormattingPolicy>(mimeTypeInheritanceChain)
-            format textStylePolicy formattingPolicy input (Selection(fromOffset, toOffset))
+        member __.FormatText(policyParent, mimeTypeInheritanceChain, input, fromOffset, toOffset) =
+            Debug.WriteLine("**Fantomas**: Formatting selection")
+            formatText policyParent mimeTypeInheritanceChain input (Selection(fromOffset, toOffset))
 
