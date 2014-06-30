414380eb53d0b578ca32b31747bd6533275108d7
Robert Jeppesen <robert.jeppesen@durius.se>
2014-07-01 00:07:53 +0200

    Add one failing and one passing test

---
+++ monodevelop/MonoDevelop.FSharpBinding/MonoDevelop.FSharp.Tests/ResolverProviderTests.fs
@@ -0,0 +1,124 @@
+﻿namespace MonoDevelop.FSharp.Tests
+open System
+open NUnit.Framework
+open MonoDevelop.FSharp
+open MonoDevelop.Core
+open MonoDevelop.Ide.Gui
+open MonoDevelop.Ide.Gui.Content
+open FSharp.CompilerBinding
+open MonoDevelop.Projects
+open MonoDevelop.Ide.TypeSystem
+open FsUnit
+open MonoDevelop.Debugger
+open ICSharpCode.NRefactory.Semantics
+open ICSharpCode.NRefactory.TypeSystem
+open MonoDevelop.FSharp.NRefactory
+
+[<TestFixture>]
+type ResolverProviderTests() =
+    inherit TestBase()
+    let mutable doc = Unchecked.defaultof<Document>
+
+    let content = """
+    let a = System.String.Concat("a","b")
+    """
+
+    let createDoc (text:string)=
+        let workbenchWindow = TestWorkbenchWindow()
+        let viewContent = new TestViewContent()
+
+        let project = new DotNetAssemblyProject ("F#", Name="test", FileName = FilePath("test.fsproj"))
+        let projectConfig = project.AddNewConfiguration("Debug")
+
+        TypeSystemService.LoadProject (project) |> ignore
+
+        viewContent.Project <- project
+
+        workbenchWindow.SetViewContent(viewContent)
+        viewContent.ContentName <- "/users/a.fs"
+        viewContent.GetTextEditorData().Document.MimeType <- "text/x-fsharp"
+        let doc = Document(workbenchWindow)
+
+        (viewContent :> IEditableTextBuffer).Text <- text
+        (viewContent:> IEditableTextBuffer).CursorPosition <- 0
+
+        let pfile = doc.Project.AddFile("/users/a.fs")
+
+        let resolverProvider = new FSharpResolverProvider()
+        viewContent.Contents.Add(resolverProvider)
+
+        try doc.UpdateParseDocument() |> ignore
+        with exn -> Diagnostics.Debug.WriteLine(exn.ToString())
+        doc
+
+    let getBasicOffset expr =
+        let startOffset = content.IndexOf (expr, StringComparison.Ordinal)
+        startOffset + (expr.Length / 2)
+
+    let resolveExpression (doc:Document, content:string, offset:int) =
+        let resolver =
+            doc.GetContents<obj>()
+            |> Seq.cast<ITextEditorResolverProvider> 
+            |> Seq.tryHead
+        
+        match resolver with
+        | Some resolver -> 
+            let region = ref Unchecked.defaultof<DomRegion>
+            let result = resolver.GetLanguageItem(doc, offset, region)
+            result
+        | None -> failwith "No debug resolver found"
+
+    let (|Typ|_|) (r: ResolveResult) =
+      match r with 
+      | :? TypeResolveResult as t -> 
+         Some t.Type
+      | _ -> None 
+
+    let (|FSharpType|_|) (r: IType) =
+      match r with 
+      | :? FSharpResolvedTypeDefinition as t -> 
+         Some t
+      | _ -> None 
+
+    let (|Member|_|) (r: ResolveResult) =
+      match r with 
+      | :? MemberResolveResult as m -> 
+         Some m.Member
+      | _ -> None 
+
+    let (|FSharpMethod|_|) (r: IMember) =
+      match r with 
+      | :? FSharpResolvedMethod as m -> 
+         Some m
+      | _ -> None 
+
+    [<TestFixtureSetUp>]
+    override x.Setup() =
+        base.Setup()
+        doc <- createDoc(content)
+
+    [<Test>]
+    member x.``String.Concat method should return correct overload``() =
+        let basicOffset = getBasicOffset (".Concat")
+        match resolveExpression (doc, content, basicOffset) with
+        | Member (FSharpMethod m) -> 
+            m.Name |> should equal "Concat" 
+            m.Parameters.Count |> should equal 2
+            m.Parameters.[0].Name |> should equal "str0"
+            m.Parameters.[0].Type.FullName |> should equal "System.String"
+            m.Parameters.[1].Name |> should equal "str1"
+            m.Parameters.[1].Type.FullName |> should equal "System.String"
+        | _ -> Assert.Fail "Not a method result"
+
+    [<Test>]
+    member x.``System.String should yield a type``() =
+        let basicOffset = getBasicOffset (".String")
+        match resolveExpression (doc, content, basicOffset) with
+        | Typ (FSharpType typ) -> 
+            typ.FullName |> should equal "System.String" 
+            typ.ParentAssembly.AssemblyName |> should equal "mscorlib"
+            typ.TypeArguments.Count |> should equal 0
+        | _ -> Assert.Fail "Not a method result"
+
+   
+
