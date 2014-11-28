810fcd95b72ecc2aa4bf2badf14749584dd23e86
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-11-28 03:04:42 +0200

    Improve configure to try F# 3 and 3.1

--- monodevelop/configure.bat
+++ monodevelop/configure.bat
@@ -1,2 +1,12 @@
-@"%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi" configure.fsx "%*"
+if exist "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" (
+	"%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi" configure.fsx "%*"
+) else (
+	if exist "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0\fsi.exe" (
+		"%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0\fsi" configure.fsx "%*"
+	) else (
+		echo "F# not installed."
+		exit
+	)
+)
+
 ..\lib\nuget\NuGet.exe restore MonoDevelop.FSharpBinding\MonoDevelop.FSharp.Windows.sln
