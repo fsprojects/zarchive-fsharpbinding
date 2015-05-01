576f914d194ee529b8b6614163b4e549c73e0702
7sharp9 <dave.thomas@xamarin.com>
2015-05-01 10:22:28 +0100

    Remove F# addin files
    F# addin has now moved to https://github.com/fsharp/FSharpMDXS

--- monodevelop/configure.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -e
-echo "Configuring..."
-fsharpi configure.fsx "$@"
-echo "Getting nuget packages..."
-mozroots --import --sync --quiet || echo 'Could not import mozroots, proceeding anyway'
-(cd MonoDevelop.FSharpBinding && mono ../../lib/nuget/NuGet.exe restore MonoDevelop.FSharp.mac-linux.sln)
